9407cc6c158e99d21273cef899326b9c9c91a0b5
programmingbunny <programmingdanielle@gmail.com>
2022-04-10 19:23:03 -0500

    unmarshal

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 )
 
 type person struct {
@@ -19,11 +21,20 @@ func main() {
 
 	xp := []person{p1, p2}
 
-	// bs, err := json.Marshal(xp)
+	bs, err := json.Marshal(xp)
 
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
+	if err != nil {
+		log.Panic(err)
+	}
 
-	fmt.Println(xp)
+	fmt.Println("PRINT JSON", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("back to go data structure", xp2)
 }
